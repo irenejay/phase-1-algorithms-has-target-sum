a49929314a7f4eec4f65cd0c065f82e078fe9390
irene <irenejim9504@gmail.com>
2024-01-25 18:44:39 +0300

    Added the implementation for the hasTargetSum function to determine whether there are two numbers in an array that sum up to a given target.

--- index.js
+++ index.js
@@ -1,34 +1,18 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
 
-/* 
-  Write the Big O time complexity of your function here
-*/
+function hasTargetSum(arr, target) {
+  const seenNumbers = new Set();
 
-/* 
-  Add your pseudocode here
-*/
+  for (const num of arr) {
+    const complement = target - num;
 
-/*
-  Add written explanation of your solution here
-*/
+    if (seenNumbers.has(complement)) {
+      return true;
+    }
 
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+    seenNumbers.add(num);
+  }
 
-  console.log("");
-
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
+  return false;
 }
 
 module.exports = hasTargetSum;
